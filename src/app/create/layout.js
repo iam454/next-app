ca13129c268fd8840c1c8b26a707a208c1373bc4
iam454 <realseogy@gmail.com>
2023-12-16 22:38:13 +0900

    feat: 라우팅 연습

---
+++ src/app/create/layout.js
@@ -0,0 +1,10 @@
+const Layout = (props) => {
+  return (
+    <div>
+      <h2>Oh my god</h2>
+      {props.children}
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/app/read/[id]/page.js
@@ -0,0 +1,10 @@
+const Read = (props) => {
+  return (
+    <div>
+      <h2>Read</h2>
+      params : {props.params.id}
+    </div>
+  );
+};
+
+export default Read;
